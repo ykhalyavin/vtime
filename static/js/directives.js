c0a1815b8ac71e17b30c74ee11ccec597f72a8e3
Yury Khalyavin <yury.khalyavin@gmail.com>
2016-03-22 12:02:33 +0300

    Codestyle

--- static/js/directives.js
+++ static/js/directives.js
@@ -76,11 +76,14 @@
                     },
                     ranges: {
                         'Today': [moment(), moment()],
-                        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                        'Yesterday': [moment().subtract(1, 'days'),
+                                      moment().subtract(1, 'days')],
                         'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                         'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                        'This Month': [moment().startOf('month'), moment().endOf('month')],
-                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                        'This Month': [moment().startOf('month'),
+                                       moment().endOf('month')],
+                        'Last Month': [moment().subtract(1, 'month').startOf('month'),
+                                       moment().subtract(1, 'month').endOf('month')]
                     }
 
                 }, cb);
